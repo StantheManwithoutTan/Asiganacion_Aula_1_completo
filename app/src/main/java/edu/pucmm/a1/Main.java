ff43cc3e0a61a42978808c0f62009a5ffed5dd6a
StantheManwithoutTan <SXGX0001@ce.pucmm.edu.do>
2025-02-03 07:01:45 -0400

    first commit (sin cambios)

---
+++ app/src/main/java/edu/pucmm/a1/Main.java
@@ -0,0 +1,43 @@
+package edu.pucmm.a1;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        var app = Javalin.create(javalinConfig ->{
+            javalinConfig.staticFiles.add("/publico");
+        })
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(7000);
+
+        // filtro before
+        app.before("/", ctx ->{
+            System.out.println("Filtro de la barra...");
+            // Validacion
+            Usuario usuario = ctx.sessionAttribute("USUARIO");
+            if (usuario == null){
+                // no ha sido encontrado
+                ctx.redirect("/formulario.html");
+            }
+            System.out.println("Usuario: "+usuario);
+        });
+
+        app.post("/autenticar", ctx -> {
+            String nombre = ctx.formParam("nombre");
+            String contrasena = ctx.formParam("password");
+
+            if(true);
+            // simplificando muchas cosas
+            Usuario usuario = new Usuario()nombre, "ICC", 1);
+            ctx.sessionAttribute("USUARIO", usuario);
+            ctx.redirect("/");
+
+        })
+
+        record Usuario(String nombre, String carrera, int id){
+
+        }
+    }
+}
+
